9bff37b6cc76bf0cb81bbbfe90884af04ff87206
lxxjn0 <ljy3510@gmail.com>
2021-02-02 23:33:03 +0900

    feat(init): kotlin-spring 프로젝트와 react-typescript 프로젝트 생성

---
+++ back/src/main/kotlin/me/lxxjn0/back/BackApplication.kt
@@ -0,0 +1,11 @@
+package me.lxxjn0.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackApplication>(*args)
+}
---
+++ back/src/test/kotlin/me/lxxjn0/back/BackApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.lxxjn0.back
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
