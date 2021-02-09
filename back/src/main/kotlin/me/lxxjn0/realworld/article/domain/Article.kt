9937d1f8dbfdb14ed86cdc44cabcbff2363304cd
lxxjn0 <ljy3510@gmail.com>
2021-02-10 00:32:18 +0900

    feat(back/article): Article Entity 구현

---
+++ back/src/main/kotlin/me/lxxjn0/realworld/article/domain/Article.kt
@@ -0,0 +1,37 @@
+package me.lxxjn0.realworld.article.domain
+
+import me.lxxjn0.realworld.common.domain.BaseEntity
+import me.lxxjn0.realworld.user.domain.User
+import javax.persistence.*
+
+@Entity
+class Article(
+    slug: String,
+    title: String,
+    description: String,
+    body: String,
+    author: User
+) : BaseEntity() {
+    @Id
+    @Column(name = "slug")
+    var slug: String = slug
+        private set
+
+    @Column(name = "title")
+    var title: String = title
+        private set
+
+    @Column(name = "description")
+    var description: String = description
+        private set
+
+    @Lob
+    @Column(name = "body")
+    var body: String = body
+        private set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "author")
+    var author: User = author
+        private set
+}
