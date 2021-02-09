45f26a8b9974f5f73cfee77a44bfdad846f8f23e
lxxjn0 <ljy3510@gmail.com>
2021-02-09 23:01:01 +0900

    feat(back/gradle): jvm 11로 버전 변경

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -3,14 +3,16 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+
     kotlin("jvm") version "1.4.21"
     kotlin("plugin.spring") version "1.4.21"
     kotlin("plugin.jpa") version "1.4.21"
+    kotlin("plugin.allopen") version "1.4.21"
 }
 
 group = "me.lxxjn0"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -24,15 +26,19 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude("junit-platform-commons")
+    }
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
     testImplementation("io.rest-assured:kotlin-extensions:4.3.3")
 }
 
 tasks.withType<KotlinCompile> {
+    sourceCompatibility = "11"
+
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
