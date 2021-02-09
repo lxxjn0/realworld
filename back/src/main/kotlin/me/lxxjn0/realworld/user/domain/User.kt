67b495334985296f7ee590ed15ba86b8299b8fc6
lxxjn0 <ljy3510@gmail.com>
2021-02-10 00:31:48 +0900

    feat(back/user): User Entity 구현

---
+++ back/src/main/kotlin/me/lxxjn0/realworld/user/domain/User.kt
@@ -0,0 +1,39 @@
+package me.lxxjn0.realworld.user.domain
+
+import me.lxxjn0.realworld.common.domain.BaseEntity
+import javax.persistence.*
+
+@Entity
+class User(
+    email: String,
+    password: String,
+    username: String,
+    bio: String,
+    image: String
+) : BaseEntity() {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    var id: Long? = null
+        private set
+
+    @Column(name = "email")
+    var email: String = email
+        private set
+
+    @Column(name = "password")
+    var password: String = password
+        private set
+
+    @Column(name = "username")
+    var username: String = username
+        private set
+
+    @Column(name = "bio")
+    var bio: String = bio
+        private set
+
+    @Column(name = "image")
+    var image: String = image
+        private set
+}
