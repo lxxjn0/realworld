afdb16bd80498585712d7a5e46b87c5d7e47f5e3
lxxjn0 <ljy3510@gmail.com>
2021-02-12 01:03:05 +0900

    feat(back/common): Slf4j 어노테이션 구현

---
+++ back/src/main/kotlin/me/lxxjn0/realworld/comment/config/Slf4j.kt
@@ -0,0 +1,13 @@
+package me.lxxjn0.realworld.comment.config
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Slf4j() {
+    companion object {
+        inline val <reified T> T.log: Logger
+            get() = LoggerFactory.getLogger(T::class.java)
+    }
+}
